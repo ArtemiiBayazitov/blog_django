38f88903ee08cb85bd55736953f2cb0eac3021dd
artemii <artemii.rgs@gmail.com>
2025-04-09 14:51:14 +0400

    registration models in admin.py

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.template.defaultfilters import title
 
-# Register your models here.
+from .models import Category, Post
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title', )}
+    list_display = ('title', 'author', 'category', 'publication_data')
