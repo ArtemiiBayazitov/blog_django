b5aff1e0028447864a17a2d2b2f2aebc2b78cc0b
ArtemiiBayazitov <162894774+ArtemiiBayazitov@users.noreply.github.com>
2025-04-16 21:32:48 +0400

    Update settings.py
    remove

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p5ei=r(hy!l#_tn0hku$k^5lq2b3d8amlgn*p+043@a0bcdrzn'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
